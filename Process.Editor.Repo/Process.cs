1e7b6ed73a9d49804374b65b620b3ec80bebbefd
Abid Mahmood <abid.mahmood@inotech.eu>
2021-02-22 15:34:14 +0100

    Repository

---
+++ Process.Editor.Repo/Process.cs
@@ -0,0 +1,30 @@
+﻿using Process.Editor.Elements;
+using ProcessManagement.DataStorage.EF;
+
+namespace Process.Editor.Repo
+{
+    public class Process
+    {
+        PMDataContext _pMDataContext = new PMDataContext();
+
+        public Process()
+        {
+            PMDataContext pMDataContext = new PMDataContext();
+        }
+
+        public void LoadProcess()
+        {
+
+        }
+
+        public void AddProcess(ProcessModel processModel)
+        {
+            _pMDataContext.Process.Add(processModel);
+        }
+
+        public void RemoveProcess(ProcessModel processModel)
+        {
+            _pMDataContext.Process.Remove(processModel);
+        }
+    }
+}
